2cc859cdabeac46f162b6182e8c3bd11c0d386d4
destx0 <destisdbest@gmail.com>
2022-07-23 00:26:25 +0530

    social

--- web/src/constants/socialLinks.js
+++ web/src/constants/socialLinks.js
@@ -4,22 +4,22 @@ import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 export const socialLinks = [
   {
     name: 'youtube',
-    url: 'https://www.youtube.com/webcifarOfficial',
+    url: 'https://www.youtube.com/channel/UCrp3auXbbpWoFE3y4oJwuAg',
     icon: <FaYoutube />,
   },
   {
     name: 'instagram',
-    url: 'https://www.youtube.com/web_cifar',
+    url: 'https://instagram.com/the_speaker_ind?igshid=YmMyMTA2M2Y=',
     icon: <FaInstagram />,
   },
   {
     name: 'facebook',
-    url: 'https://www.facebook.com/webcifar',
+    url: 'https://www.facebook.com/The-Speaker-100100148004919',
     icon: <FaFacebook />,
   },
-  {
-    name: 'twitter',
-    url: 'https://www.twitter.com/webcifar',
-    icon: <FaTwitter />,
-  },
+  // {
+  //   name: 'twitter',
+  //   url: 'https://www.twitter.com/',
+  //   icon: <FaTwitter />,
+  // },
 ];
