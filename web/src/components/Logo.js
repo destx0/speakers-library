3f2b7a55c1070e277193cbda00488029add8ec53
destx0 <destisdbest@gmail.com>
2022-07-22 15:58:37 +0530

    header

---
+++ web/src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FcBookmark } from 'react-icons/fc';
+import LogoStyles from '../styles/LogoStyles';
+
+function Logo() {
+  return (
+    <LogoStyles to="/">
+      <FcBookmark />
+    </LogoStyles>
+  );
+}
+
+export default Logo;
