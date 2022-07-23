1f73110b6974f6351a18c8663c441cd055e323a0
destx0 <destisdbest@gmail.com>
2022-07-23 12:04:53 +0530

    featured section

--- web/src/styles/typography/SectionTitleStyles.js
+++ web/src/styles/typography/SectionTitleStyles.js
@@ -4,7 +4,7 @@ export const SectionTitleStyles = styled.h2`
   font-size: 3rem;
   font-family: 'Poppins', sans-serif;
   font-weight: 600;
-  color: var(--primary);
+  color: var(--white);
   @media only screen and (max-width: 768px) {
     font-size: 2.5rem;
   }
