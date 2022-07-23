d48561be3bb149e0caa6acb3bcb952f373af82b1
destx0 <destisdbest@gmail.com>
2022-07-23 13:40:23 +0530

    404 done

--- web/src/styles/category/CategoryGridStyles.js
+++ web/src/styles/category/CategoryGridStyles.js
@@ -5,9 +5,9 @@ export const CategoryGridStyles = styled.div`
   margin-top: 3.5rem;
   display: grid;
   gap: 5rem;
-  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   @media only screen and (max-width: 768px) {
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
     gap: 2rem;
   }
 `;
