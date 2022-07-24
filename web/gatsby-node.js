2f230339ee44c16995a50ac25249b6ab09909f2a
destx0 <destisdbest@gmail.com>
2022-07-24 12:47:32 +0530

    single category

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -2,6 +2,9 @@ exports.createPages = async ({ graphql, actions }) => {
   const postsPerPage = parseInt(process.env.GATSBY_POST_PER_PAGE) || 10;
   const singleBlogTemplate = require.resolve('./src/templates/single-blog.js');
   const blogListTemplate = require.resolve('./src/templates/blog-list.js');
+  const singleCategoryTemplate = require.resolve(
+    './src/templates/single-category.js'
+  );
   const { createPage } = actions;
   const result = await graphql(`
     {
@@ -13,10 +16,26 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
+      allSanityCategory {
+        nodes {
+          id
+          slug {
+            current
+          }
+        }
+      }
+      allSanityAuthor {
+        nodes {
+          id
+          slug {
+            current
+          }
+        }
+      }
     }
   `);
   const blogs = result.data.allSanityBlog.nodes;
-
+  const categories = result.data.allSanityCategory.nodes;
   //   single blog page
   blogs.forEach((blog) => {
     createPage({
@@ -37,6 +56,14 @@ exports.createPages = async ({ graphql, actions }) => {
         numberOfPages: totalBlogListPages,
         currentPage: index + 1,
       },
+    });
+  });
+  // single category pages
+  categories.forEach((category) => {
+    createPage({
+      path: `/categories/${category.slug.current}`,
+      component: singleCategoryTemplate,
+      context: { id: category.id },
     });
   });
 };
