10eb119922713319053b537c2744bf2990341cfa
destx0 <destisdbest@gmail.com>
2022-07-24 15:25:26 +0530

    final

---
+++ web/src/styles/goalStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const goalStyles = styled.div`
+  text-align: center;
+  .title {
+    font-size: 3rem;
+  }
+
+  .link {
+    color: var(--primary);
+    text-decoration: none;
+    :hover {
+      text-decoration: underline;
+    }
+  }
+`;
