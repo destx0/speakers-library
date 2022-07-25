f1a03c1531cb3cbfb5aa3c84d9dea5890ffe9ecb
destx0 <destisdbest@gmail.com>
2022-07-25 18:39:13 +0530

    sentry

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -9,13 +9,13 @@ module.exports = {
     description: `speakers-library is a platform for knowledge in your native language`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-image',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-source-sanity',
+      resolve: "gatsby-source-sanity",
       options: {
         ...sanityConfig,
       },
@@ -27,11 +27,18 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-social9-socialshare',
+      resolve: "gatsby-plugin-social9-socialshare",
       options: {
-        content: 'da2f2c59832a4614a34f76e5cffb1501',
+        content: "da2f2c59832a4614a34f76e5cffb1501",
         async: true,
         defer: true,
+      },
+    },
+    {
+      resolve: "@sentry/gatsby",
+      options: {
+        dsn: "https://6d27c855d2524aadb473bb437b6e631d@o1333822.ingest.sentry.io/6599588",
+        sampleRate: 0.7,
       },
     },
   ],
