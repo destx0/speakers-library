f824fff245e8348f3fa8d741a6e17776a350878f
destx0 <destisdbest@gmail.com>
2022-07-25 22:39:48 +0530

    pace install

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -61,5 +61,13 @@ module.exports = {
         headerHeight: 0,
       },
     },
+    {
+      resolve: 'gatsby-plugin-pace',
+      options: {
+        theme: 'minimal',
+        color: 'white',
+        cdn: 'https://cdn.bootcdn.net/ajax/libs/pace/1.0.2/pace.min.js',
+      },
+    },
   ],
 };
